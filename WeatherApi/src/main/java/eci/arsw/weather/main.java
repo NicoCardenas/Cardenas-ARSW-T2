40fd23290965acd3991a885a52196bc216a70d0c
NicoCardenas <nicolas.cardenas@mail.escuelaing.edu.co>
2019-03-28 08:59:46 -0500

    problems with map.js

---
+++ WeatherApi/src/main/java/eci/arsw/weather/main.java
@@ -0,0 +1,14 @@
+package eci.arsw.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan
+public class main {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(main.class, args);
+    }
+}
